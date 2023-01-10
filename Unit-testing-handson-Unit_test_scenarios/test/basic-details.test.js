1f15f6dfe2a85a8e11e7943a676b55ea682d550d
Debanjan-Mukherjee <83631050+Debanjan-Mukherjee@users.noreply.github.com>
2023-01-10 12:51:41 +0530

    Update basic-details.test.js

--- Unit-testing-handson-Unit_test_scenarios/test/basic-details.test.js
+++ Unit-testing-handson-Unit_test_scenarios/test/basic-details.test.js
@@ -1,36 +1,37 @@
 import { html, fixture, expect } from '@open-wc/testing';
+import { Sinon } from 'sinon';
 import '../src/LoanBasicDetails/BasicDetails.js';
-import Sinon from 'sinon';
+import { BasicDetails } from '../src/LoanBasicDetails/BasicDetails.js';
 
 
-describe('BasicDetails.js', () => {
-  const el = await fixture(html`<basic-details></basic-details>`);
-  const form = el.shadowRoot.querySelectorAll('lion-button');
-  //const basicDetail = new BasicDetails();
-  it('check for spy', () => {
-    const abc = Sinon.spy(el, "_toDashboard");
+describe('Basic details', async () => {
+  // Write test cases inside this block
+  // refer basic-details.js files
+  let element = await fixture(html`<basic-details></basic-details>`);
+  let form = element.shadowRoot.querySelectorAll('lion-button');
+  it('checks for spy', () => {
+    let checkSpy = Sinon.spy(element, "_toDashboard");
     form[0].click();
-    expect(abc.calledOnce).to.be.true;
+    expect(checkSpy.calledOnce).to.be.true;
   });
 
-  it('check accessible', () => {
-    expect(el).to.be.accessible;
+  it('is accessible', () => {
+    expect(element).to.be.accessible;
   });
 
-  it('check for capture', () => {
-    const fun = Sinon.spy(el, "_captureDetails");
+  it('checks for capture', () => {
+    let captureDetails = Sinon.spy(element, "_captureDetails");
     form[1].click();
-    expect(fun.calledOnce).to.be.true;
+    expect(captureDetails.calledOnce).to.be.true;
   });
 
-
-  it('number to words', () => {
-    const val = 10005;
+  it('converts number to word', () => {
+    let val = 10005;
     let num = 10005;
-    // const func = el._numToWord(val,num);
-    el._numToWord(val, num);
-    //console.log(func);
+    element._numToWord(val, num);
     expect(num).to.equal(10005.00);
-  });
+  })
 
+
+  
 });
