56e405da21dcb34ca916be37cbf5b1c49b360eb0
Debanjan-Mukherjee <83631050+Debanjan-Mukherjee@users.noreply.github.com>
2023-01-10 12:54:03 +0530

    Update loan-application.test.js

--- Unit-testing-handson-Unit_test_scenarios/test/loan-application.test.js
+++ Unit-testing-handson-Unit_test_scenarios/test/loan-application.test.js
@@ -2,20 +2,24 @@ import { html, fixture, expect } from '@open-wc/testing';
 
 import '../loan-application.js';
 
+
+
 describe('LoanApplication', () => {
   // Write test cases inside this block
-  it('check for accessibility' , async () => {
-    const el = await fixture(html`<loan-application></loan-application>`);
-    expect(el).to.be.accessible;
-  });
-  it('Check title', async () => {
-    const el = await fixture(html`<loan-application></loan-application>`);
-    expect(el.title).to.equal('Hey there');
-    expect(el.counter).to.equal(5);
+  it('checks for accessibility', async () => {
+    let loanApplicationElement = await fixture(html`<loan-application></loan-application>`);
+    expect(loanApplicationElement).to.be.accessible;
   });
 
-  it('Is custom element dashboard accessible', async () => {
-    const el = await fixture(html`<dash-board></dash-board>`);
-    await expect(el).to.be.accessible;
+  it('checks title', async () => {
+    let loanApplicationElement = await fixture(html`<loan-application></loan-application>`);
+    expect(loanApplicationElement.title).to.equal('Hey there');
+    expect(loanApplicationElement.counter).to.equal(5);
   });
+
+  it('checks dashboard custom element accessibilty', async () => {
+    let dashboardElement = await fixture(html`<dash-board></dash-board>`);
+    expect(dashboardElement).to.be.accessible;
+  });
+  
 });
