4daed0d1632c00ae58db405af507b617c172b20e
Debanjan-Mukherjee <83631050+Debanjan-Mukherjee@users.noreply.github.com>
2023-01-10 12:53:35 +0530

    Update headers.test.js

--- Unit-testing-handson-Unit_test_scenarios/test/headers.test.js
+++ Unit-testing-handson-Unit_test_scenarios/test/headers.test.js
@@ -1,19 +1,23 @@
 import { html, fixture, expect } from '@open-wc/testing';
-import { stub } from 'sinon';
+import { isTemplatePartActive } from 'lit-html';
+import { Sinon } from 'sinon';
 import '../src/header/Header.js';
-import Sinon  from 'sinon';
 
-const el = await fixture(html`<loan-header></loan-headers>`);
-const form = el.shadowRoot.querySelectorAll('button');
-describe('loan-header', () => {
-  
-  it('check for spy', () => {
-    const abc = Sinon.spy(el, "localeChanged");
+
+
+describe('loan-header', async () => {
+  // Write test cases inside this block
+  let element = await fixture(html`<loan-header></loan-header>`);
+let form = element.shadowRoot.querySelectorAll('button');
+  it('checks for spy', () => {
+    const spyCheck = Sinon.spy(element, "localeChanged");
     form[0].click();
-    expect(abc.calledOnce).to.be.true;
+    expect(spyCheck.calledOnce).to.be.true;
   });
 
-it('check accessible', () => {
-  expect(el).to.be.accessible;
-});
+  it('checks accessible', () => {
+    expect(element).to.be.accessible;
+  })
+
+  
 });
