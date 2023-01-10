da76426e7dd7811743c0adb5a9020aa36a75570b
Debanjan-Mukherjee <83631050+Debanjan-Mukherjee@users.noreply.github.com>
2023-01-10 12:54:44 +0530

    Update loan-emi-details.test.js

--- Unit-testing-handson-Unit_test_scenarios/test/loan-emi-details.test.js
+++ Unit-testing-handson-Unit_test_scenarios/test/loan-emi-details.test.js
@@ -1,26 +1,28 @@
 import { html, fixture, expect } from '@open-wc/testing';
 import '../src/LoanEMIDetails/LoanEMIDetails.js';
+import { Sinon } from 'sinon';
+import { isTemplatePartActive } from 'lit-html';
 
-import Sinon from 'sinon';
 
-const el = await fixture(html`<loanemi-details></loanemi-details>`);
-const form = el.shadowRoot.querySelectorAll('lion-button');
 
-describe('Loan EMI details', () => {
-  it('check customer function', () => {
-    const abc = Sinon.spy(el, "_toCustomer");
+describe('Loan EMI details', async () => {
+  // Write test cases inside this block
+  let element = await fixture(html`<loanemi-details></loanemi-details>`);
+  let form = element.shadowRoot.querySelectorAll('lion-button');
+  it('checks for spy of _toCustomer', () => {
+    let checkSpy = Sinon.spy(element, '_toCustomer');
     form[0].click();
-    expect(abc.calledOnce).to.be.true;
+    expect(checkSpy.calledOnce).to.be.true;
   });
 
-  it('check customer function', () => {
-    const abc = Sinon.spy(el, "_toBasicDetails");
+  it('checks for spy of _toBasicDetails', () => {
+    let checkSpy = Sinon.spy(element, '_toBasicDetails');
     form[0].click();
-    expect(abc.calledOnce).to.be.true;
+    expect(checkSpy.calledOnce).to.be.true;
   });
-  
 
-  it('check accessible', () => {
-    expect(el).to.be.accessible;
+  it('checks accessible', () => {
+    expect(element).to.be.accessible;
   });
+
 });
